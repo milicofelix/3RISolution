aa532847f06f31a5b06f55dcfa2a21febb962b1a
Adriano Bradbury <milicofelix@hotmail.com>
2015-10-06 16:15:02 -0300

    Classe Corretoras finalizada, porém com uma dúvida que será tirada posteriormente.

---
+++ Classes/Corretoras.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adriano
+ * Date: 06/10/2015
+ * Time: 15:38
+ */
+
+namespace Classes;
+
+
+use Classes\Config\Conexao;
+
+class Corretoras
+{
+    private $conn;
+
+    public function __construct(){
+
+        $this->listAnalises = new Analise();
+        $this->conn         = Conexao::getInstance();
+    }
+
+    private function getListaCorretoras($request)
+{
+
+    if( !$this->conn )
+{
+    echo "Nao pude recuperar Lista de Corretoras por nao conseguir conexao com o banco COTACOES";
+
+    return "";
+}
+
+        $xml = "";
+
+		$statementCotacoes = null;
+		$rs = null;
+
+		try
+        {
+//            Set datas = new TreeSet<String>(); faltando implemntar....
+
+			$sql = "SELECT cd_corretora, nm_corretora FROM corretora ORDER BY nm_corretora ASC ";
+
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchObject();
+
+			$xml .="<corretoras>";
+
+            foreach($result as $rs)
+            {
+                $xml .= "<corretora>";
+                $xml .= $rs->nm_corretora . " (" . $rs->cd_corretora . ")";
+                $xml .= "</corretora>";
+            }
+
+			$xml .= "</corretoras>";
+            
+		}
+        catch(\PDOException $e)
+		{
+            echo "Erro ao tentar recuperar lista de corretoras.";
+        }
+
+		return $xml;
+	}
+
+}
