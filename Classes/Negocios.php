c1e7bad1554eb0c5b8e7a279092f89b448cef514
Adriano Bradbury <milicofelix@hotmail.com>
2015-10-07 16:33:54 -0300

    Classe Negocios migrada de Java para PHP, ainda faltam alguns ajustes.

--- Classes/Negocios.php
+++ Classes/Negocios.php
@@ -26,7 +26,7 @@ class Negocios
 
 if( !$this->conn )
 {
-    echo "Nao pude recuperar Negócios por nao conseguir conexao com o banco COTACOES");
+    echo "Nao pude recuperar Negócios por nao conseguir conexao com o banco COTACOES";
 
     return "";
 }
@@ -44,175 +44,123 @@ if( !$this->conn )
 				$delay = $request->getParameter("delay");
 				$data  = $request->getParameter("data");
 
-				String sql = "SELECT dh, preco, qtd, cd_corr_compra, cd_corr_venda, seq, vft " +
-                "FROM " + ($bolsa.equals("2") ? "NEGOCIO_BMF" : "NEGOCIO_BOVESPA") + " " +
-                "WHERE codigo = '" + ativo + "' AND " +
-                "dh::date = '" + $data + "' " +
-                ($delay.equals("1") ? "AND dh < now() - interval '00:15:00' " : "") +
+				$sql = "SELECT dh, preco, qtd, cd_corr_compra, cd_corr_venda, seq, vft " .
+                "FROM " . ($bolsa == "2") ? "NEGOCIO_BMF" : "NEGOCIO_BOVESPA" . " " .
+                "WHERE codigo = ? AND dh::date = ? " .
+                ($delay == "1") ? "AND dh < now() - interval '00:15:00' " : "" .
                 "ORDER BY seq DESC";
 
-				statementCotacoes = connectionCotacoes.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bindValue(1,$ativo);
+                $stmt->bindValue(2,$data);
+                $stmt->execute();
 
-				rs = statementCotacoes.executeQuery(sql);
-
-				if( !rs.next() )
+				if( !$rs = $stmt->nextRowset() )
                 {
-                    sql = "SELECT dh, preco, qtd, cd_corr_compra, cd_corr_venda, seq, vft " +
-                        "FROM " + ($bolsa.equals("2") ? "NEGOCIO_BMF_PERIODO" : "NEGOCIO_BOVESPA_PERIODO") + " " +
-                        "WHERE codigo = '" + ativo + "' AND " +
-                        "dh::date = '" + $data + "' " +
-                        ($delay.equals("1") ? "and dh < now() - interval '00:15:00' " : "") +
+                    $sql = "SELECT dh, preco, qtd, cd_corr_compra, cd_corr_venda, seq, vft " .
+                        "FROM " . ($bolsa == "2") ? "NEGOCIO_BMF_PERIODO" : "NEGOCIO_BOVESPA_PERIODO" . "".
+                        "WHERE codigo = '" . $ativo . 
+                        "' AND " ."dh::date = '" . $data . "' " .
+                        ($delay == "1") ? "and dh < now() - interval '00:15:00' " : "" .
                         "ORDER BY seq DESC";
-                    rs = statementCotacoes.executeQuery(sql);
+                    
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bindValue(1,$ativo);
+                $stmt->bindValue(2,$data);
+                $stmt->execute();
                 }
+                
                 else
-                    rs.beforeFirst();
+                    $rs->beforeFirst();///Verifica no PHP a forma de aplicar essa função########################################################################################################################
 
-				sb.append("<negocios>");
+				$xml .= "<negocios>";
 
-				while( rs.next() )
+				while( $rs->next() )
                 {
-                    sb.append("<negocio>");
-                    sb.append("<d>" + rs.getString("dh") + "</d>");
-                    sb.append("<p>" + rs.getString("preco") + "</p>");
-                    sb.append("<q>" + rs.getString("qtd") + "</q>");
-                    sb.append("<c>" + rs.getString("cd_corr_compra") + "</c>");
-                    sb.append("<v>" + rs.getString("cd_corr_venda")  + "</v>");
-                    sb.append("<s>" + rs.getString("seq") + "</s>");
-                    sb.append("<t>" + rs.getString("vft") + "</t>");
-                    sb.append("</negocio>");
+                    $xml .= "<negocio>";
+                    $xml .= "<d>" . $rs->dh . "</d>";
+                    $xml .= "<p>" . $rs->preco . "</p>";
+                    $xml .= "<q>" . $rs->qtd . "</q>";
+                    $xml .= "<c>" . $rs->cd_corr_compra . "</c>";
+                    $xml .= "<v>" . $rs->cd_corr_venda  . "</v>";
+                    $xml .= "<s>" . $rs->seq . "</s>";
+                    $xml .= "<t>" . $rs->vft . "</t>";
+                    $xml .= "</negocio>";
                 }
 
-				sb.append("</negocios>");
+				$xml .= "</negocios>";
 
-				xml = sb.toString();
 			}
-            //else
-            //	echo"Não pude recuperar negócios por não estar conectado ao banco Cotações");
+
         }
-        catch(SQLException e)
+        catch(\PDOException $e)
 		{
             //e.printStackTrace();
-            echo"Erro ao tentar recuperar negocios");
+            echo"Erro ao tentar recuperar negocios";
         }
-		finally
-		{
-            try
-            {
-                rs.close();
-            }
-            catch(Exception e)
-			{
-                e.printStackTrace();
-            }
 
-			try
-            {
-                statementCotacoes.close();
-            }
-            catch(Exception e)
-			{
-                e.printStackTrace();
-            }
-
-			//try
-			//{
-			//	connectionCotacoes.close();
-			//}
-			//catch(Exception e)
-			//{
-			//	e.printStackTrace();
-			//}
-		}
-
-		return xml;
+		return $xml;
 	}
 
-    private String get$datasNegocios(HttpServletRequest request)
+    private function getdatasNegocios($request)
 	{
         //Connection connectionCotacoes = conexaoBancoCotacoes();
 
-        if( !conexaoBancoCotacoes() )
+        if( !$this->conn )
         {
-            echo"Nao pude recuperar Negócios por nao conseguir conexao com o banco COTACOES");
+            echo "Nao pude recuperar Negócios por nao conseguir conexao com o banco COTACOES";
             return "";
         }
 
-        String xml = "";
-		StringBuilder sb = new StringBuilder();
+        $xml = "";
 
-		Statement statementCotacoes = null;
-		ResultSet rs = null;
+		$statementCotacoes = null;
+		$rs = null;
 
 		try
         {
             //if( connectionCotacoes != null && !connectionCotacoes.isClosed() /*&& statementCotacoes != null*/ )
             {
-                Set $datas = new TreeSet<String>();
+                $datas = new \ArrayObject();//Set datas = new TreeSet<String>(); Verificar como implementar no PHP###########################################################
 
-				String sql = "SELECT $data FROM negocio_$data ORDER BY $data DESC ";
-				statementCotacoes = connectionCotacoes.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-				rs = statementCotacoes.executeQuery(sql);
-				while( rs.next() )
-                    $datas.add(rs.getString("$data"));
+				$sql = "SELECT data FROM negocio_data ORDER BY data DESC ";
 
-				sql = "SELECT dh::date AS $data FROM negocio_bovespa ORDER BY $data DESC ";
-				rs = statementCotacoes.executeQuery(sql);
-				while( rs.next() )
-                    $datas.add(rs.getString("$data"));
+                $stmt = $this->conn->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetchObject();
 
-				sb.append("<$datas>");
+                foreach($result as $rs)
+                    $datas->add($rs->data);
 
-				for( Iterator i = $datas.iterator(); i.hasNext(); )
+				$sql = "SELECT dh::date AS data FROM negocio_bovespa ORDER BY data DESC ";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->execute();
+                $result = $stmt->fetchObject();
+
+                foreach($result as $rs)
+                    $datas->add($rs->data);
+
+				$xml .= "<$datas>";
+
+				for( $i = $datas->iterator(); $i->hasNext(); )
 				{
-                    sb.append("<$data>");
-                    sb.append(i.next());
-                    sb.append("</$data>");
+                    $xml .= "<data>";
+                    $xml .= $i->next();
+                    $xml .= "</data>";
                 }
 
-				sb.append("</$datas>");
-
-				xml = sb.toString();
+				$xml .= "</datas>";
 			}
             //else
             //	echo"Não pude recuperar $datas de negócios por não estar conectado ao banco Cotações");
         }
-        catch(SQLException e)
+        catch(\PDOException $e)
 		{
             //e.printStackTrace();
-            echo"Erro ao tentar recuperar $datas de negocios");
+            echo "Erro ao tentar recuperar datas de negocios";
         }
-		finally
-		{
-            try
-            {
-                rs.close();
-            }
-            catch(Exception e)
-			{
-                e.printStackTrace();
-            }
 
-			try
-            {
-                statementCotacoes.close();
-            }
-            catch(Exception e)
-			{
-                e.printStackTrace();
-            }
-
-			//try
-			//{
-			//	connectionCotacoes.close();
-			//}
-			//catch(Exception e)
-			//{
-			//	e.printStackTrace();
-			//}
-		}
-
-		return xml;
+		return $xml;
 	}
 
 }
